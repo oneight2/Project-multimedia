451f1cc24c5c25918bd9e3ea431e746e6a0bde62
oneight2 <1606109@sttgarut.ac.id>
2020-11-09 05:58:42 +0700

    template anyar

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -6,7 +6,20 @@ class Home extends CI_Controller {
 	public function index() {
 		$data = array(
 			'title' => "Home",
+			'departemen' => $this->db->get('departemen')->result_array(),
+			'jenis_project' => $this->db->get('project')->result_array(),
 		);
+
+		// $this->form_validation->set_rules('nama', 'Nama', 'trim|required');
+		// $this->form_validation->set_rules('nama_project', 'Nama Project', 'trim|required');
+		// $this->form_validation->set_rules('deskripsi', 'Deskripsi Project', 'trim|required');
+		// $this->form_validation->set_rules('target', 'Target Waktu Project', 'trim|required');
+		// $this->form_validation->set_rules('link', 'Link Contoh Project', 'trim|required');
+		// $this->form_validation->set_rules('link', 'Link Contoh Project', 'trim|required');
+		// if ($this->form_validation->run() == TRUE or FALSE) {
 		$this->load->view('dist/home', $data);
+		// } else {
+		// 	# code...
+		// }
 	}
 }
